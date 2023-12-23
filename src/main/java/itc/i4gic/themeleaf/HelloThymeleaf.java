4e55842e79b1517ff60585ca379d926edb3d5c91
Phearun Ol <phearun.ph77@gmail.com>
2023-12-23 20:39:08 +0700

    :truck: Test thymeleaf with js css

--- src/main/java/itc/i4gic/themeleaf/HelloThymeleaf.java
+++ src/main/java/itc/i4gic/themeleaf/HelloThymeleaf.java
@@ -6,11 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 
+
 @Controller
 public class HelloThymeleaf {
    @GetMapping("/hello")
    public String hello(Model model) {
       model.addAttribute("message", "Hello world");
        return "helloworld";
+   }
+
+   @GetMapping("/hi")
+   public String task01(Model model) {
+      model.addAttribute("message", "Hello Task01");
+       return "hi";
+   }
+   
+   @GetMapping("/style")
+   public String task02() {
+       return "html-css-js";
    }
 }
